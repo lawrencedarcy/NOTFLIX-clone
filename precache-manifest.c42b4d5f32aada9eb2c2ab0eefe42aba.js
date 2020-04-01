7a270817ae5f11ff9e325de5ec09d5423b5bf1ed
lawrencedarcy <lawrencedwakefield@gmail.com>
2020-04-01 15:33:19 +0100

    Updates

---
+++ precache-manifest.c42b4d5f32aada9eb2c2ab0eefe42aba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1680358792651845f8f7efa3734b76f",
+    "url": "/netflix-react-exercise/index.html"
+  },
+  {
+    "revision": "6da403c2fa9467c7970e",
+    "url": "/netflix-react-exercise/static/css/main.d3d40328.chunk.css"
+  },
+  {
+    "revision": "aafd86ed1ea1df9aec6f",
+    "url": "/netflix-react-exercise/static/js/2.a7381e94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/netflix-react-exercise/static/js/2.a7381e94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6da403c2fa9467c7970e",
+    "url": "/netflix-react-exercise/static/js/main.91ad54dd.chunk.js"
+  },
+  {
+    "revision": "fd53a5c63842701c13af",
+    "url": "/netflix-react-exercise/static/js/runtime-main.09b2f664.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/netflix-react-exercise/static/media/logo.5d5d9eef.svg"
+  }
+]);
